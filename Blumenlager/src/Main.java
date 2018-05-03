41cc63f874045fc66ab433b1ea3af1d79cd04810
degcd <38460594+degcd@users.noreply.github.com>
2018-05-03 14:17:20 +0200

    mySQL Datenbankanbindung (#4)

--- Blumenlager/src/Main.java
+++ Blumenlager/src/Main.java
@@ -1,8 +1,10 @@
 import Datenhaltung.ArtikelDAO;
+
 import Datenhaltung.RegalDAO;
 import Fachlogik.Artikelverwaltung.Artikelverwaltung;
 import Fachlogik.Lagerverwaltung.Regalverwaltung;
 import UI.Controller;
+import java.sql.*;
 
 public class Main {
 	public static void main(String[] args) {	
@@ -13,6 +15,26 @@ public class Main {
 		//Test; Aufruf sp�ter �ber Hauptmenue
 		controller.zeigeEinlagernView();
 		controller.zeigeAuslagernView();
+		
+		
+		//Test Datenbank
+		try{
+			//Get a connection to database
+			Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/blumenlager", "blumenlager", "blumenlager");
+			//Create a statement
+			Statement mystat = myConn.createStatement();
+			//Execute Sql query
+			ResultSet myRs = mystat.executeQuery("select * from artikel");
+			//Process the result set
+			while(myRs.next())
+			{
+				System.out.println(myRs.getString("idartikel")+ " "
+						+ myRs.getString("bezeichnung"));
+			}
+		}
+		catch(Exception ex){
+			
+		}
 
 	}
 
