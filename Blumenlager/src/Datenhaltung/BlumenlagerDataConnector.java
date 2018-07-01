bb9bbf473a15e592220cb1b2ccc4f78df02f939d
degcd <degcd@vi200825.vi.corp>
2018-07-01 16:33:44 +0200

    Parametrisierung-BlumenlagerDataConnector

--- Blumenlager/src/Datenhaltung/BlumenlagerDataConnector.java
+++ Blumenlager/src/Datenhaltung/BlumenlagerDataConnector.java
@@ -1,5 +1,8 @@
 package Datenhaltung;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
@@ -9,7 +12,18 @@ public class BlumenlagerDataConnector implements IDataConnector {
 	
 	public BlumenlagerDataConnector(){
 		try{
-			this.conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/blumenlager", "blumenlager", "blumenlager");
+			
+			FileReader fr = new FileReader("BlumenlagerDBConnConfig.txt");
+			BufferedReader br = new BufferedReader(fr);
+			
+			String url = br.readLine();
+			String user = br.readLine();
+			String password = br.readLine();
+			
+			this.conn = DriverManager.getConnection(url, user, password);
+			
+			br.close();
+			
 		}catch(Exception e){}
 	}
 	
